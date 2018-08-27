03b337f07f12f5a754af30a90e982629dc546b1c
Juan Carlos Buendia <jcbuendia@iMac-de-Admin.local>
2018-08-27 11:48:57 -0500

    second commit

--- construye.sh
+++
@@ -1,6 +0,0 @@
-echo "# react-app-clima" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/TDSjcbp123/react-app-clima.git
-git push -u origin master
